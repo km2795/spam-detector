891d8a175b74453185f4075aca82178268e577fe
Kunal Sharma <kunalkm2795@gmail.com>
2024-10-05 08:43:54 +0530

    Put the model testing code.

--- spam-detector/spam_detector.py
+++ spam-detector/spam_detector.py
@@ -10,7 +10,7 @@ MAIN_DIR = config("MAIN_DIR")
 MODEL_DUMP_DIRECTORY = MAIN_DIR + "/spam-detector/spam_detector_model_dump_dir"
 
 
-print("Preparing dataset...\n\n")
+print("\n\nPreparing dataset...\n\n")
 
 # Prepare the dataset.
 ( train_set_input,
@@ -25,10 +25,19 @@ model_input_size = len(train_set_input[0])
 print("\n\nTraining model...\n\n")
 
 # Train the model.
-model_accuracy = sd_train.train_model(model_input_size, train_set_input,
-  train_set_output, test_set_input, test_set_output, MODEL_DUMP_DIRECTORY)
+model_accuracy = sd_train.train_model(model_input_size, 
+                                      train_set_input,
+                                      train_set_output, 
+                                      test_set_input, 
+                                      test_set_output, 
+                                      MODEL_DUMP_DIRECTORY)
 
 print("\n\nTesting model...\n\n")
 
+# Test the model.
+test_result = sd_test.test_model(test_set_input, 
+                                 test_set_output, 
+                                 MODEL_DUMP_DIRECTORY)
+
 # How the model performed.
-print("\n\nModel Accuracy: ", model_accuracy, "\n\n")
+print("\n\nModel Accuracy: ", test_result, "\n\n")
