c77c5226dfbee311245b078c8f40422975b03912
Kunal Sharma <kunalkm2795@gmail.com>
2024-10-07 08:39:16 +0530

    Turn off information log.

--- spam-detector/spam_detector.py
+++ spam-detector/spam_detector.py
@@ -1,4 +1,13 @@
+import os
 from decouple import config
+
+# Turn off information log.
+# 0 = all messages are logged (default behavior)
+# 1 = INFO messages are not printed
+# 2 = INFO and WARNING messages are not printed
+# 3 = INFO, WARNING, and ERROR messages are not printed
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 import dataset_parser as dataset_parser
 import spam_detector_train as sd_train
