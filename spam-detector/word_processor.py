5a81713a03dc78f60604547d726f56bdf6c0dcf4
Kunal Sharma <kunalkm2795@gmail.com>
2025-04-06 22:38:14 +0530

    Refactor
    Improved the function:
    1. Removes whitespace, empty, new line (character only) strings.
    2. Added more checks to make it faster.

--- spam-detector/word_processor.py
+++ spam-detector/word_processor.py
@@ -7,26 +7,39 @@ stemmer = PorterStemmer()
 # Word lemmatizing algorithm.
 lemmatizer = WordNetLemmatizer()
 
-# Utility to perform word stemming or lemmatization.
+"""
+Utility to perform word stemming and/or 
+lemmatization along with general cleaning.
+"""
 def process_word(stem, lemma, words, delim):
+  # If string contains only new or a space character or is empty, then
+  # return empty string.
+  if (words == '\n' or words == '' or words == ' '):
+    return ""
+
   # If argument passed is a string or not,
   # Need to return it as the same type.
   str_flag = False
+  processed_words = []
 
   # See if there is need to split the input or not.
-  if (type(words) == str):
+  if (type(words) == str and delim is not None):
     words = words.split(delim)
     str_flag = True
 
   # Perform word stemming and lemmatization together.
   if stem and lemma:
-    do_stem = [stemmer.stem(o) for o in words]
-    do_lemma = [lemmatizer.lemmatize(item) for item in do_stem]
-    processed_words = do_lemma
+    for word in words:
+      if word is not ' ' and word is not '':
+        word = stemmer.stem(word)
+        word = lemmatizer.lemmatize(word)
+        processed_words.append(word)
   
-  # Perform either stemming or lemmatization only.
+  # Perform either stemming or lemmatization (by-default) only.
   else:
-    processed_words = [stemmer.stem(item) for item in words] if stem else [lemmatizer.lemmatize(item) for item in words]
+    for word in words:
+      if word is not ' ' and word is not '':
+        processed_words.append(stemmer.stem(word) if stem else lemmatizer.lemmatize(word))
 
   # Return as a string if received as a string.
-  return " ".join(processed_words) if str_flag else processed_words
+  return "".join(processed_words) if str_flag else processed_words
